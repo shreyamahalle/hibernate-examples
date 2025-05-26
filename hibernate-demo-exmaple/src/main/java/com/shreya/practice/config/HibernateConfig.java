adb7ca68fe8eac3507a3857bb25168e9c94505aa
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-26 16:04:29 +0530

    updated

--- hibernate-demo-exmaple/src/main/java/com/shreya/practice/config/HibernateConfig.java
+++ hibernate-demo-exmaple/src/main/java/com/shreya/practice/config/HibernateConfig.java
@@ -1,4 +1,16 @@
 package com.shreya.practice.config;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 public class HibernateConfig {
+
+    private static SessionFactory buildSessionFactory(){
+
+        try {
+            return new Configuration().configure().buildSessionFactory();
+        }
+        finally {
+
+        }
+    }
 }
