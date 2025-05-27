e0c0c7eb64488fd72f7fb08176b75e9425eb8b9e
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-27 16:23:21 +0530

    changes added

--- hibernate-demo-exmaple/src/main/java/com/shreya/practice/controller/StudentController.java
+++ hibernate-demo-exmaple/src/main/java/com/shreya/practice/controller/StudentController.java
@@ -1,4 +1,40 @@
 package com.shreya.practice.controller;
 
+import com.shreya.practice.model.Student;
+import com.shreya.practice.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/studentManagement")
 public class StudentController {
+
+    @Autowired
+    private StudentService studentService;
+
+    @PostMapping
+    public ResponseEntity<Student> saveStudent(@RequestBody Student student){
+        Student savedStudent = studentService.saveStudent(student);
+        return ResponseEntity.ok(savedStudent);
+    }
+    @GetMapping("/students")
+    public ResponseEntity<List<Student>> getAllStudents(){
+        List<Student> students = studentService.getAllStudents();
+        return ResponseEntity.ok(students);
+    }
+    @GetMapping("/student/{id}")
+    public ResponseEntity<Student> getStudentById(@PathVariable int id){
+        Optional<Student> student = studentService.getStudentById(id);
+        return student.map(ResponseEntity::ok).orElseGet(()-> ResponseEntity.notFound().build());
+    }
+    @DeleteMapping("/student/{id}")
+    public ResponseEntity<Student> deleteStudentById(@PathVariable int id){
+        Student deletedStudent = studentService.deleteStudentById(id);
+        return (deletedStudent != null) ? ResponseEntity.ok(deletedStudent) : ResponseEntity.notFound().build();
+    }
+
 }
