260c54aff1f2747dbb71ee583904a24b0f8cf39b
shreya mahalle <shreyamahalle2000@gmail.com>
2025-05-27 16:35:03 +0530

    changes added

--- hibernate-demo-exmaple/src/main/java/com/shreya/practice/HibernateDemoExampleApplication.java
+++ hibernate-demo-exmaple/src/main/java/com/shreya/practice/HibernateDemoExampleApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HibernateDemoExmapleApplication {
+public class HibernateDemoExampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(HibernateDemoExmapleApplication.class, args);
+		SpringApplication.run(HibernateDemoExampleApplication.class, args);
 	}
 
 }
